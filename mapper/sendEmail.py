30c6085f8eaca6ff31fefd7194bfac7c0572497c
LinXiaoDe <qdl.cs@qq.com>
2021-02-13 13:06:45 +0800

    update port and timeout

--- mapper/sendEmail.py
+++ mapper/sendEmail.py
@@ -4,11 +4,12 @@ from email.mime.text import MIMEText
 
 def sendEmail(sender,receivers,AuthCode,fromUser,toUser,head,content):
     # 请自行修改下面的邮件发送者和接收者
+    port = 587                      # stmp使用端口
     message = MIMEText(content, _subtype='plain', _charset='utf-8')
     message['From'] = Header(fromUser, 'utf-8')
     message['To'] = Header(toUser, 'utf-8')
     message['Subject'] = Header(head, 'utf-8')
-    smtper = SMTP('smtp.qq.com')
+    smtper = SMTP('smtp.qq.com',port=port,timeout=10)
 
     # QQ邮箱smtp的授权码
     smtper.login(sender, AuthCode)  
