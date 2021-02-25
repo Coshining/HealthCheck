3870e659b351d72a8b1fbaf5f5c6a17730820505
LinXiaoDe <qdl.cs@qq.com>
2021-02-25 09:56:29 +0800

    update readData.py

--- mapper/readData.py
+++ mapper/readData.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 def readData(dataPath):
-    data=pd.read_csv(dataPath,header=None,names=["username","passwd","email","RealAddress","RealCity","RealCounty","RealProvince"])
+    data=pd.read_csv(dataPath,header=None,names=["username","passwd","email","RealAddress","RealCity","RealCounty","RealProvince","IsInCampus"])
     return data.values
 
 if __name__ == '__main__':
     data = readData("../data/user.csv")
+    print(data)
